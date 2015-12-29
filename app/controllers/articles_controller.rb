3587dc1ad54af2546c83746d9cfd160166ca6920
Doug Woodward <woodw116@mail.chapman.edu>
2015-12-28 19:39:30 -0800

    Initial Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+	def show
+    @article = Article.find(params[:id])
+    end
+
+	def new
+	end
+
+	def create
+		@article = Article.new(articles_params)
+ 
+  		@article.save
+  		redirect_to @article
+	end
+	private
+	 def articles_params
+	 	params.require(:article).permit(:title, :text)
+	 end
+end
